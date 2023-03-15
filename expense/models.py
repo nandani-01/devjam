0eb0a557e7db99f2e765066497c87dbb23a23735
hiralgabhane <44315059+hiralgabhane@users.noreply.github.com>
2023-03-15 20:32:53 +0300

    Add files via upload

---
+++ expense/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+# class ExpenseRecord(models.Model):
+# 	date = models.DateField()
+# 	amount = models.DecimalField(max_digits=8, decimal_places=2)
+# 	description = models.CharField(max_length=200)
